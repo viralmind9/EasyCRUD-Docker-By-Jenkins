729e32c33be17cc51b7095f91da56de5245db4ad
Faizan Mansuri <faizanahmedmansuri@gmail.com>
2026-02-20 15:19:54 +0530

    Add Terraform variables for instance type, AMI, and region

---
+++ Terraform/variable.tf
@@ -0,0 +1,12 @@
+variable "instance_type" {
+    type = string
+    default = "c7i-flex.large"
+}
+variable "ami" {
+    type = string
+    default = "ami-073130f74f5ffb161"
+}
+variable "region" {
+    type = string
+    default = "eu-north-1"
+}
